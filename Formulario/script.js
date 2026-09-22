65272b6e067914cba8977e187a114a9f0f53e590
Thalyson Viana <thalysonviana2029@gmail.com>
2026-09-22 08:53:25 -0300

    Update script.js

--- Formulario/script.js
+++ Formulario/script.js
@@ -1,54 +1,45 @@
 function mostrarNumero(){
 
-    const nome = document.getElementById("nome");
-    const numero1 = Number(document.getElementById(n1));
-    const numero2 = Number(document.getElementById(n2));
-    const numero3 = Number(document.getElementById(n3));
-    const numero4 = Number(document.getElementById(n4));
-    const numero5 = Number(document.getElementById(n5));
+  let numeros = [];
 
-    const total = (numero1 + numero2 + numero3 + numero4 + numero5);
+   numeros.push(Number(document.getElementById("n1").value));
+   numeros.push(Number(document.getElementById("n2").value));
+   numeros.push(Number(document.getElementById("n3").value));
+   numeros.push(Number(document.getElementById("n4").value));
+   numeros.push(Number(document.getElementById("n5").value));
 
-    let situacao
+   let maior = numeros [0];
+   let menor = numeros [0];
 
-    if(numero1 > numero2){
-        situacao = "Numero 1 é o maior";
- 
-    } else if(numero2 > numero3){
-        situacao = "Numero 2 é o maior";
- 
-    } else if(numero3 > numero4){
-        situacao = "Numero 1 é o maior";
- 
-    } else if(numero4 > numero5){
-        situacao = "Numero 1 é o maior";
- 
-    } else {
-        situacao = "Esse numero é menor";
-    }
+   let pares = 0, impares = 0, soma = 0;
 
-    const num1 = (numero1 / 2);
-    const num2 = (numero2 / 2);
-    const num3 = (numero3 / 2);
-    const num4 = (numero4 / 2);
-    const num5 = (numero5 / 2);
-    
+   for (let i = 0; i < numeros.length; i++ ){
+        let numero = numeros[i];
 
-    if((num1 == 0) || (num2 == 0 ) || (num3 == 0) || (num4 == 0) || (num5 == 0)){
-        situacao = "Esse número é par";
-    } else if((num1 = 1) || (num2 = 1) || (num3 = 1) || (num4 = 1) || (num5 = 1)){
-        situacao = "Esse número é ímpar";
-}
+      if(numero>maior){
+        maior = numero
+      }
+      
+      if(numero<menor){
+        menor = numero
+      }
 
-    document.getElementById("number").textContent = 
+      if(numero % 2 === 0){
+        pares++
+      }else{
+        impares++
+      }
 
-"Nome : " + nome +
-"Numero 1: " + numero1 +
-"Numero 2: " + numero2 +
-"Numero 3: " + numero3 +
-"Numero 4: " + numero4 +
-"Numero 5: " + numero5 +
-"Total: " + total +
-"Situação: " + situacao
+      soma = soma + numero;
 
-}
+   }
+
+   
+   document.getElementById("number").textContent = 
+   " |maior: " + maior +
+   " |menor: " + menor +
+   " |impares: " + impares +
+   " |pares: " + pares +
+   " |soma: " + soma
+
+   }
